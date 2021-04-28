32df4d0f8c53faf42c477bd270ceb93456079971
Maity <sourav.maity@epsilon.com>
2021-04-28 09:30:21 +0530

    first commit

---
+++ vpc_module/main.tf
@@ -0,0 +1,45 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block       = var.vpc_cidr
+    instance_tenancy = "default"
+
+    tags = {
+        Name = "MyVPC-created-using-module",
+        CreatedBy = "Sourav"
+    }
+}
+
+data "aws_availability_zones" "all_azs" {
+  
+}
+
+# output "all_avail_zones" {
+#   value = data.aws_availability_zones.all_azs
+# }
+resource "aws_subnet" "my_private_subnet" {
+    # count = length(data.aws_availability_zones.all_azs.names)
+    count = length(var.subnet_cidr)
+    vpc_id     = aws_vpc.myvpc.id
+    # cidr_block = var.private_subnet_cidr[count.index]
+    # cidr_block = "172.20.${count.index}.0/24"
+    cidr_block = var.subnet_cidr[count.index]
+    availability_zone = data.aws_availability_zones.all_azs.names[count.index]
+    # tags = {
+    #     Name = "MyPrivateSubnet_${count.index + 1}",
+    #     CreatedBy = "Sourav"
+    # }
+    tags = var.tag_names
+}
+
+# data "aws_vpc" "myvpc" {
+#   id = aws_vpc.myvpc.id
+# }
+
+# resource "aws_subnet" "my_private_subnet_123" {
+#     vpc_id     = data.aws_vpc.myvpc.id
+#     cidr_block = "172.20.2.0/24"
+
+#     tags = {
+#         Name = "MyPrivateSubnet3",
+#         CreatedBy = "Sourav"
+#     }
+# }
---
+++ vpc_module/variables.tf
@@ -0,0 +1,12 @@
+variable "tag_names" {
+  description = "Tag names for networking resources"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+}
+
+variable "subnet_cidr" {
+  description = "List of CIDR for subnets"
+}
+
